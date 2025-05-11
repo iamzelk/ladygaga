bf14e0de0e036c5d6ab93446a6b47f5e724f4ffe
Zelk <66845093+iamzelk@users.noreply.github.com>
2025-05-11 13:02:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             title: 'The Fame',
             year: 2008,
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Video116/v4/12/e9/1e/12e91eaf-1ed9-c38f-a0cb-5134d425adab/Jobedf0124b-9306-45a7-8d8b-e6373cf1bd6d-154984021-PreviewImage_preview_image_nonvideo_sdr-Time1693423091097.png/316x316bb.jpg',
+            cover: 'img/the_fame.jpg',
             audioFiles: ['01-just_dance.mp3', '02-lovegame.mp3', '03-paparazzi.mp3', '04-poker_face.mp3'],
             trackTitles: ['Just Dance', 'LoveGame', 'Paparazzi', 'Poker Face'],
             artist: 'Lady Gaga',
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             title: 'The Fame Monster',
             year: 2009,
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/5b/a4/0a/5ba40a95-9bf6-3b0c-ee7b-82f0bf71f589/10UMGIM00578.rgb.jpg/316x316bb.jpg',
+            cover: 'img/the_fame_monster.jpg',
             audioFiles: ['01-bad_romance.mp3', '02-alejandro.mp3', '03-monster.mp3', '06-telephone.mp3'],
             trackTitles: ['Bad Romance', 'Alejandro', 'Monster', 'Telephone'],
             artist: 'Lady Gaga',
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             title: 'Born This Way',
             year: 2011,
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Video115/v4/01/f8/75/01f875d6-19fb-b068-c730-853c6b98669a/Jobb2d6eca9-9008-4dd1-b1e8-6b6c70c7a9c3-114006709-PreviewImage_preview_image_nonvideo_sdr-Time1621641746478.png/316x316bb.jpg',
+            cover: 'img/born_this_way.jpg',
             audioFiles: ['01-marry_the_night.mp3', '02-born_this_way.mp3', '04-judas.mp3', '14-the_edge_of_glory.mp3'],
             trackTitles: ['Marry The Night', 'Born This Way', 'Judas', 'The Edge Of Glory'],
             artist: 'Lady Gaga',
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             title: 'ARTPOP',
             year: 2013,
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/0c/33/64/0c336478-aaa9-95dc-d373-f6fb8510b170/13UAAIM69752.rgb.jpg/316x316bb.jpg',
+            cover: 'img/ARTPOP.jpg',
             audioFiles: ['01-aura.mp3', '02-venus.mp3', '03-g_u_y.mp3', '14-applause.mp3'],
             trackTitles: ['Aura', 'Venus', 'G.U.Y', 'Applause'],
             artist: 'Lady Gaga',
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             title: 'Joanne',
             year: 2016,
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/c7/79/a9/c779a941-7961-7dfa-99b8-0a99ebbfd1b8/16UMGIM68302.rgb.jpg/316x316bb.webp.',
+            cover: 'img/joanne.jpg',
             audioFiles: ['02-a_yo.mp3', '04-john_wayne.mp3', '05-dancin_ in_circles.mp3', '07-million_reasons.mp3'],
             trackTitles: ['A-YO', 'John Wayne', 'Dancin In Circles', 'Million Reasons'],
             artist: 'Lady Gaga',
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             title: 'Chromatica',
             year: 2020,
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/5d/4d/f9/5d4df96a-e95e-2fe9-404a-5d570513762d/20UMGIM15390.rgb.jpg/316x316bb.webp',
+            cover: 'img/chromatica.jpg',
             audioFiles: ['03-stupid_love.mp3', '04-rain_on_me.mp3', '08-911.mp3', '12-replay.mp3'],
             trackTitles: ['Stupid Love', 'Rain On Me', '911', 'Replay'],
             artist: 'Lady Gaga',
@@ -81,7 +81,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             title: 'MAYHEM',
             year: 2025,
-            cover: 'https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/85/96/e5/8596e5c9-ea5d-f074-14ab-0def74d49a41/Job55a60f4d-834e-43b4-b0ac-bc65708cbd78-187464330-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_366977506_2078765148-Time1741332248310.png/316x316bb.jpg',
+            cover: 'img/MAYHEM.jpg',
             audioFiles: ['01-disease.mp3', '02-abracadabra.mp3', '04-perfect_celebrity.mp3', '05-vanish_into_you.mp3'],
             trackTitles: ['Disease', 'Abracadabra', 'Perfect Celebrity', 'Vanish Into You'],
             artist: 'Lady Gaga',
